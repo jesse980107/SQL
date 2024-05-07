438106815ccfeab6ada763200aec0f302c116d73
Muxi Jin <jmx1998@student.ubc.ca>
2024-05-07 15:08:55 -0700

    1327 two version Advanced String Functions / Regex / Clause

---
+++ SQL 50/1327#.sql
@@ -0,0 +1,15 @@
+select product_name ,sum(unit) as unit
+from Products
+join Orders using(product_id)
+where MONTH(order_date) = 2 AND YEAR(order_date) = 2020
+group by product_name
+having sum(unit)>=100
+
+--------------------------
+# Write your MySQL query statement below
+SELECT 
+    p.product_name,
+    SUM(o.unit) AS unit
+FROM Products p
+INNER JOIN Orders o ON p.product_id = o.product_id AND MONTH(o.order_date) = 2 AND YEAR(order_date) = 2020
+GROUP BY o.product_id HAVING unit >= 100;
