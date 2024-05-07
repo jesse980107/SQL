a987dfb90e558c4072c62ae362bc0de8f02cef05
Muxi Jin <jmx1998@student.ubc.ca>
2024-05-06 18:02:02 -0700

    1789 v2

--- SQL 50/1789#.sql
+++ SQL 50/1789#.sql
@@ -7,3 +7,14 @@ where a.primary_flag = "Y" or employee_id in (
     having count(department_id) = 1
 )
 
+--this is better since it avoids potential pitfalls
+--with the IN subquery performance issues in the first solution.
+SELECT employee_id, department_id
+FROM Employee 
+WHERE primary_flag = 'Y'
+UNION
+SELECT employee_id, department_id
+FROM Employee 
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1
+ORDER BY employee_id;
