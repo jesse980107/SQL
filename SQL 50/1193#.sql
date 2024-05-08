69c6f037d10f9c43e5726b91a5c3388defdcd329
Muxi Jin <jmx1998@student.ubc.ca>
2024-05-07 18:10:18 -0700

    1193

--- SQL 50/1193#.sql
+++ SQL 50/1193#.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT  DATE_FORMAT(trans_date,'%Y-%m') month, country, count(*) as trans_count ,
+select  DATE_FORMAT(trans_date,'%Y-%m') month, country, count(*) as trans_count ,
 sum(if(state="approved",1,0)) as approved_count ,
 sum(amount) as  trans_total_amount,
 sum(CASE WHEN state = 'approved' then amount else 0 END) as approved_total_amount
