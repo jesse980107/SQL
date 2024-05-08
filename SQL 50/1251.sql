5e02522d3b1b573648b098328d7661e50f57ab83
Muxi Jin <jmx1998@student.ubc.ca>
2024-05-07 17:50:28 -0700

    1251

---
+++ SQL 50/1251.sql
@@ -0,0 +1,5 @@
+select p.product_id ,IFNULL(round(sum(price*units)/sum(units),2),0) as average_price 
+from Prices p
+left join UnitsSold u 
+on p.product_id     = u.product_id     and purchase_date between start_date and end_date   
+group by p.product_id    
