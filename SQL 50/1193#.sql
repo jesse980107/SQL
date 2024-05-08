3d0f419dd4d4ae67a6b1d93f043b6b6c35d46236
Muxi Jin <jmx1998@student.ubc.ca>
2024-05-07 18:09:56 -0700

    1193

--- SQL 50/1193#.sql
+++ SQL 50/1193#.sql
@@ -2,6 +2,6 @@
 SELECT  DATE_FORMAT(trans_date,'%Y-%m') month, country, count(*) as trans_count ,
 sum(if(state="approved",1,0)) as approved_count ,
 sum(amount) as  trans_total_amount,
-SUM(CASE WHEN state = 'approved' then amount else 0 END) as approved_total_amount
+sum(CASE WHEN state = 'approved' then amount else 0 END) as approved_total_amount
 from Transactions
 group by month,country 
