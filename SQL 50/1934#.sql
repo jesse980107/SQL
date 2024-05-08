ed7ab34fabf329a00efd1c44997c82c83cfd5a96
Muxi Jin <jmx1998@student.ubc.ca>
2024-05-07 17:25:29 -0700

    1934 fix

--- SQL 50/1934#.sql
+++ SQL 50/1934#.sql
@@ -1,4 +1,4 @@
-select s.user_id, sum(if(action="confirmed",1,0))/count(*) as confirmation_rate 
+select s.user_id, round(sum(if(action="confirmed",1,0))/count(*),2) as confirmation_rate 
 from Signups s
 left join Confirmations c using(user_id)
 group by s.user_id  
