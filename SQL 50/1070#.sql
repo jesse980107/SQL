e45766fc71c4f0aae330cac64e6479fffd9cd387
Muxi Jin <jmx1998@student.ubc.ca>
2024-05-26 14:35:44 -0700

    1070

--- SQL 50/1070#.sql
+++ SQL 50/1070#.sql
@@ -7,3 +7,11 @@ with firstyear as(
 select s.product_id, s.year as first_year, s.quantity, s.price 
 from Sales s
 join firstyear on firstyear.product_id = s.product_id  and s.year = firstyear.fy;
+--------------------------------------
+
+select product_id, s.year as first_year,quantity, price
+from Sales s
+where (s.year,s.product_id) in (
+    select min(year) as year,product_id
+    from Sales 
+    group by product_id) ;
