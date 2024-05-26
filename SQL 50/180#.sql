82b3d45bac910ea775f5b9d9e9fcdd2fcfe4e04b
Muxi Jin <jmx1998@student.ubc.ca>
2024-05-26 14:22:02 -0700

    180

--- SQL 50/180#.sql
+++ SQL 50/180#.sql
@@ -23,3 +23,9 @@ WITH temporary_table(num, next_num, prev_num) AS (
 SELECT DISTINCT num AS ConsecutiveNums 
 FROM temporary_table 
 WHERE num = next_num AND num = prev_num;
+---------------------
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 ON l1.id = l2.id - 1
+JOIN Logs l3 ON l2.id = l3.id - 1
+WHERE l1.num = l2.num AND l2.num = l3.num;
